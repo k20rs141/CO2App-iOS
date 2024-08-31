bc08f25477e8f165a356a0994a8c390d2b95d2e0
Koki Yamada <koki.yamada@link-u.co.jp>
2024-08-31 23:54:51 +0900

    Add: WeatherView

---
+++ CO2App-iOS/CO2App-iOS/WeatherView.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherView.swift
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        VStack {
+            Text("weather view")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.ultraThinMaterial)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
